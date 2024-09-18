eda2aba22622a036dfca56fb5ccc7d57c45f1e76
SudiptaRoy05 <sudiptaroyballave121@gmail.com>
2024-09-18 13:22:47 +0600

    dom added

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const images = document.getElementsByTagName('img');
+console.log(images);
+
+const cards = document.getElementsByClassName('card');
+console.log(cards);
+
+const htmlTag = document.getElementsByTagName('html')[0];
+console.log(htmlTag)
+
+// htmlTag.setAttribute('data-theme','dark');
+htmlTag.setAttribute('data-theme','light');
+
+const btns = document.getElementsByClassName('btn')[0];
+console.log(btns);
+
